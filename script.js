17c975e0afd83867afa8696028fd383210f79b43
mnubhan <nubhant@gmail.com>
2023-07-13 21:59:18 +0100

    feat:make adjustment to script.js and style.css file

--- script.js
+++ script.js
@@ -7,18 +7,19 @@ let weather = {
       .then((response) => response.json())
       .then((data) => this.displayWeather(data));
   },
-  displayWeather: function(data){
-    const {name} = data;
-    const {icon, description} = data.weather[0];
-    const {temp, humidity} =data.main;
-    const {speed} = data.wind;
-    console.log(name, icon, description, temp, humidity, speed)
-    document.querySelector(".city").innerText = `Weather in ${name}`
-    document.querySelector(".icon").src = `https://openweathermap.org/img/wn/${icon}.png`
-    document.querySelector(".description").innerText = description
-    document.querySelector(".temp").innerText = `${temp}°C`
-    document.querySelector(".humidity").innerText = `Humidity: ${humidity}%`
-    document.querySelector(".wind").innerText = `Wind speed: ${speed} km/h`
-
-  }
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, icon, description, temp, humidity, speed);
+    document.querySelector(".city").innerText = `Weather in ${name}`;
+    document.querySelector(
+      ".icon"
+    ).src = `https://openweathermap.org/img/wn/${icon}.png`;
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = `${temp}°C`;
+    document.querySelector(".humidity").innerText = `Humidity: ${humidity}%`;
+    document.querySelector(".wind").innerText = `Wind speed: ${speed} km/h`;
+  },
 };
